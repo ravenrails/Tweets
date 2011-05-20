a2cce64ab37bdf4ed7d4a239d14b167cae76619e
Alvin Garcia <alvindgarcia@yahoo.com>
2011-05-20 16:46:24 +0800

    Changed title

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
 set :application, "tweets"
-set :repository,  "set your repository location here"
+set :repository,  "https://github.com/ravenrails/Tweets.git"
 
 set :scm, :git
-set :deploy_to, "/home/exist/ruby/rails"
+set :deploy_to, "/home/exist/ruby/rails/cap"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+default_run_options[:pty] = true
 server "localhost", :app, :web, :db, :primary => true
 
 
@@ -12,10 +13,10 @@ server "localhost", :app, :web, :db, :primary => true
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
